406e723781c68f65c46ce585dd9c33bc1feb0346
Marcus Naufel <mvnaufel@gmail.com>
2021-08-06 17:08:56 -0300

    Aprendendo template strings

--- 2206-fundamentos-js-master/template-strings.js
+++ 2206-fundamentos-js-master/template-strings.js
@@ -1,11 +1,9 @@
-const nome = "Ju";
-const idade = 2021-1981;
-const cidadeDeNascimento = "São Paulo";
+const nome = "Marcus";
+const idade = 2021-1984;
+const cidadeDeNascimento = "Goiania";
 
 // const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
 
 const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
 
 console.log(apresentacao)
-
-
