bff93ac2d7bed9be4287d1b0c66d15a3966ed9b2
Marcus Naufel <mvnaufel@gmail.com>
2021-08-06 18:34:01 -0300

    Aprendendo parâmetros

--- 2206-fundamentos-js-master/parametros.js
+++ 2206-fundamentos-js-master/parametros.js
@@ -1,43 +1,32 @@
-// parâmetros de função
+//Parametros de função
 
-              //2       //2
-// function soma(numero1, numero2) {
-//   return numero1 + numero2;
-// }
+//function soma(num1, num2) {
+//    return num1 + num2;
+//}
 
-// console.log(soma(2, 2))
-// console.log(soma(245, 20))
-// console.log(soma(-500, 60))
+//function somaOutroNumero () {
+//    return 50 + 25
+//}
 
-// parâmetros x argumentos
+//console.log(soma(2, 2));
+//console.log(soma(4, 2));
+//console.log(soma(-500, 2)); //ordem dos parâmetros 
 
-// ordem dos parâmetros
+// parâmetros x argumentos 
 
 function nomeIdade(nome, idade) {
-  return `meu nome é ${nome} e minha idade é ${idade}`;
+    return `meu nome é ${nome} e minha idade é ${idade}`
 }
 
-// console.log(nomeIdade(40, "Juliana"))
+//console.log(nomeIdade("Marcus", 36));
 
 function soma(numero1, numero2) {
-  return numero1 + numero2;
+    return numero1 + numero2;
 }
 
-function multiplica(numero1 = 1, numero2 = 1) {
-  return numero1 * numero2;
+
+function multiplica(numero1 = 1 , numero2 = 1) {
+    return numero1 * numero2;
 }
 
-                            //9
 console.log(multiplica(soma(4, 5)))
-
-
-
-
-
-
-
-
-
-
-
-
